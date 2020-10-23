a0a1beb414998fc991959bae8e146f5c8cb6df67
Kevin <kevin.herklotz@ricardo.ch>
2020-10-23 16:01:57 +0200

    add typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,4 +49,4 @@ export default function Home() {
 // Create login page
 // Setup database
 // Add notifications
-// Make it a pwa
+// Make it a pwa // https://itnext.io/pwa-with-next-js-create-next-app-in-2020-%EF%B8%8F-9ee0e1a6313d
